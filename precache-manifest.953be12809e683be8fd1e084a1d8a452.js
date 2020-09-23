fbafb33d1fc8b63b4ca080fec500b4ea9369e1d1
wilsonwuux <71638140+wilsonwuux@users.noreply.github.com>
2020-09-22 22:42:37 -0600

    Updates

---
+++ precache-manifest.953be12809e683be8fd1e084a1d8a452.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd6ec1973f581018f895d3e28c58582",
+    "url": "/forstores/index.html"
+  },
+  {
+    "revision": "8d6c850cf20a140be982",
+    "url": "/forstores/static/css/2.1edd36c8.chunk.css"
+  },
+  {
+    "revision": "8d6c850cf20a140be982",
+    "url": "/forstores/static/js/2.21c9d3b6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/forstores/static/js/2.21c9d3b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "772e19f3fabcde9afc61",
+    "url": "/forstores/static/js/main.8e889790.chunk.js"
+  },
+  {
+    "revision": "e85a4814fb52b9365f9c",
+    "url": "/forstores/static/js/runtime-main.25279a2a.js"
+  }
+]);
